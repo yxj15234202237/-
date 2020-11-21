9809782e92068d770b75f5796ee238d072ee82b3
mgh <206399709@qq.com>
2020-11-21 19:27:01 +0800

    马国辉

--- src/components/xiaoguotushouye.vue
+++ src/components/xiaoguotushouye.vue
@@ -1,1 +1,34 @@
-效果图首页
+<template>
+  <div class="box">
+    <header>
+      <div class="heaader_left">
+      <img src="/logo.png" alt="">
+      </div>
+      <h1>效果图</h1>
+      <van-grid>
+        <van-grid-item icon="manager-o" text="我的" />
+        <van-grid-item icon="wap-nav" text="导航" />
+      </van-grid>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 100%;
+  height: 100%;
+  & > header {
+    width: 100%;
+    height: 100px;
+    display:flex;
+    justify-content: space-around;
+    & > .header_left {
+      width: 40%;
+    }
+  }
+}
+</style>
